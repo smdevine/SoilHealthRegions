01e5231b2c6aa1f03293c8c8ff29fb5551002482
smdevine <smdevine@ucdavis.edu>
2019-05-07 17:36:42 -0700

    just a bit of a clean-up

--- ssurgo_exploratory.R
+++ ssurgo_exploratory.R
@@ -1,3 +1,4 @@
+#left off at line 171
 mainDir <- 'C:/Users/smdevine/Desktop/PostDoc'
 ecoDir <- file.path(mainDir, 'soil health/ecoregions') #need to rename directory to postdoc to match laptop
 ssurgoDir <- file.path(mainDir, 'soil health/ssurgo_data')
@@ -10,6 +11,19 @@ library(aqp)
 #demo(aqp)
 #demo(slope_effect_hz_thickness)
 library(raster)
+
+#define some functions
+concat_names <- function(x, decat=FALSE) {
+  if (decat) {x <- unlist(strsplit(x, split = '-'))}
+  if(all(is.na(x))) {
+    NA
+  } else if (length(unique(x[!is.na(x)]))==1) {
+        unique(x[!is.na(x)])
+    } else {
+        paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')
+    }
+}
+
 
 #read in map unit (mu) tabular data
 mu_data <- read.csv(file.path(ssurgoDir, 'ca_mapunit_data.csv'), stringsAsFactors = FALSE)
@@ -52,7 +66,7 @@ length(unique(restrictions$cokey))
 restrictions_Fresno <- restrictions[restrictions$cokey %in% comp_data_Fresno$cokey, ]
 restrictions$mukey <- comp_data$mukey[match(restrictions$cokey, comp_data$cokey)]
 #make this just from Fresno dataset
-reskinds_by_cokey <- data.frame(cokey = as.numeric(row.names(tapply(restrictions_Fresno$reskind, restrictions_Fresno$cokey,  function(x) unique(x)))), reskinds = as.character(tapply(restrictions_Fresno$reskind, restrictions_Fresno$cokey, function(x) if(all(is.na(x))) {NA} else if (length(unique(x[!is.na(x)]))==1) {unique(x[!is.na(x)])} else{paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')})), stringsAsFactors = FALSE) #function(x) unique(x)))) #function(x) if(all(is.na(x))) {NA} else if (length(unique(x[!is.na(x)]))==1) {unique(x[!is.na(x)])} else{paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')})
+reskinds_by_cokey <- data.frame(cokey = as.numeric(row.names(tapply(restrictions_Fresno$reskind, restrictions_Fresno$cokey,  function(x) unique(x)))), reskinds = as.character(tapply(restrictions_Fresno$reskind, restrictions_Fresno$cokey, concat_names)), stringsAsFactors = FALSE) #function(x) unique(x)))) #function(x) if(all(is.na(x))) {NA} else if (length(unique(x[!is.na(x)]))==1) {unique(x[!is.na(x)])} else{paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')})
 dim(reskinds_by_cokey) #768 cokeys with restrictions
 length(unique(reskinds_by_cokey$cokey))
 lapply(reskinds_by_cokey, class)
@@ -74,14 +88,18 @@ length(unique(comp_data_Fresno$cokey)) #3563 unique components
 unique(comp_data_Fresno$taxorder) #6 soil orders here
 length(unique(comp_data_Fresno$compname)) #235 unique component names
 unique(comp_data_Fresno$taxgrtgroup)
-majcomps_no_by_mukey <- tapply(comp_data_Fresno$majcompflag, comp_data_Fresno$mukey, function(x) sum(x=='Yes'))
-majcompnames_by_mukey <- tapply(comp_data_Fresno$compname[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], function(x) paste(x, sep = ','))
+majcomps_no_by_mukey <- data.frame(mukey=row.names(tapply(comp_data_Fresno$majcompflag, comp_data_Fresno$mukey, function(x) sum(x=='Yes'))), majcomp_no = as.numeric(tapply(comp_data_Fresno$majcompflag, comp_data_Fresno$mukey, function(x) sum(x=='Yes'))), stringsAsFactors = FALSE)
+unique(majcomps_no_by_mukey$majcomp_no)
+
+majcompnames_by_mukey <- data.frame(mukey=row.names(tapply(comp_data_Fresno$compname[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], concat_names)), majcompnames=as.character(tapply(comp_data_Fresno$compname[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], concat_names)), stringsAsFactors = FALSE)
+unique(majcompnames_by_mukey$majcompnames) #277 unique major component name combos
+
 majcokeys_by_mukey <- tapply(comp_data_Fresno$cokey[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], function(x) x)
-majcomp_taxorders_by_mukey <- tapply(comp_data_Fresno$taxorder[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], function(x) if(all(is.na(x))) {NA} else if (length(unique(x[!is.na(x)]))==1) {unique(x[!is.na(x)])} else{paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')})
-domcomp_pct_by_mukey <- tapply(comp_data_Fresno$comppct_r, comp_data_Fresno$mukey, function(x) max(x, na.rm=TRUE))
-majcomp_taxorders_by_mukey
-comp_data_Fresno[comp_data_Fresno$mukey==467132, ] #example of 3 major component map unit
-class(majcomp_taxorders_by_mukey$`2562658`)
+
+majcomp_taxorders_by_mukey <- data.frame(mukey=row.names(tapply(comp_data_Fresno$taxorder[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], concat_names)), taxorders=as.character(tapply(comp_data_Fresno$taxorder[comp_data_Fresno$majcompflag=='Yes'], comp_data_Fresno$mukey[comp_data_Fresno$majcompflag=='Yes'], concat_names)), stringsAsFactors = FALSE)
+
+domcomp_pct_by_mukey <- data.frame(mukey=row.names(tapply(comp_data_Fresno$comppct_r, comp_data_Fresno$mukey, function(x) max(x, na.rm=TRUE))), docomppct=as.numeric(tapply(comp_data_Fresno$comppct_r, comp_data_Fresno$mukey, function(x) max(x, na.rm=TRUE))), stringsAsFactors = FALSE)
+
 
 #read in horizon data
 horizon_data <- read.csv(file.path(ssurgoDir, 'ca_horizon_data.csv'), stringsAsFactors = FALSE)
@@ -120,25 +138,35 @@ fresno_mu_aea$majcomps_no <- as.numeric(majcomps_no_by_mukey[match(fresno_mu_aea
 round(tapply(fresno_mu_aea$area_ac, fresno_mu_aea$majcomps_no, sum), 0)
 #0       1        2        3 
 #1742 2,230,725  661,125  409,675  (67.5% of survey area has 1 major component per map unit, 20.0% has 2 major components, 12.4% has 3 major components)
-fresno_mu_aea$taxorders <- as.character(majcomp_taxorders_by_mukey[match(fresno_mu_aea$mukey, row.names(majcomp_taxorders_by_mukey))])
+fresno_mu_aea$taxorders <- majcomp_taxorders_by_mukey$taxorders[match(fresno_mu_aea$mukey, majcomp_taxorders_by_mukey$mukey)]
 unique(fresno_mu_aea$taxorders)
-taxorders_area <- data.frame(taxorders=row.names(tapply(fresno_mu_aea$area_ac, fresno_mu_aea$taxorders, sum)), acres=as.numeric(tapply(fresno_mu_aea$area_ac, fresno_mu_aea$taxorders, sum)))
+taxorders_area <- data.frame(taxorders=row.names(tapply(fresno_mu_aea$area_ac, fresno_mu_aea$taxorders, sum)), acres=as.numeric(tapply(fresno_mu_aea$area_ac, fresno_mu_aea$taxorders, sum)), stringsAsFactors = FALSE)
+sum(taxorders_area$acres) #3259727 less than above because NA class is dropped (see next 2 calcs)
+sum(fresno_mu_aea$area_ac) - sum(taxorders_area$acres) #43540
+sum(fresno_mu_aea$area_ac[is.na(fresno_mu_aea$taxorders)]) #43540.03
+taxorders_area[nrow(taxorders_area)+1, 'taxorders'] <- 'Undefined'
+taxorders_area[nrow(taxorders_area), 'acres'] <- sum(fresno_mu_aea$area_ac[is.na(fresno_mu_aea$taxorders)]) 
 taxorders_area <- taxorders_area[order(taxorders_area$acres, decreasing=TRUE), ]
+taxorders_area
 write.csv(taxorders_area, file.path(summaryDir, 'taxorders_area.csv'), row.names=FALSE)
-fresno_mu_aea$domcomp_pct <- as.numeric(domcomp_pct_by_mukey[match(fresno_mu_aea$mukey, row.names(majcomps_no_by_mukey))])
+fresno_mu_aea$domcomp_pct <- domcomp_pct_by_mukey$docomppct[match(fresno_mu_aea$mukey, majcomps_no_by_mukey$mukey)]
 
-#this calc ok now I think - 5/6/19
+
 summary(as.factor(tapply(reskinds_by_cokey$cokey, reskinds_by_cokey$mukey, function(x) length(x))))
+#reskinds needs to be unconcatenated first before finding unique
+reskinds_by_mukey <- data.frame(mukey = row.names(tapply(reskinds_by_cokey$cokey, reskinds_by_cokey$mukey, function(x) unique(x))), reskinds = as.character(tapply(reskinds_by_cokey$reskind, reskinds_by_cokey$mukey, concat_names, decat=TRUE)), stringsAsFactors = FALSE)
+unique(reskinds_by_mukey$reskinds) #now this appears to be ok
 
-#no longer coercing mukey to numeric
-reskinds_by_mukey <- data.frame(mukey = row.names(tapply(reskinds_by_cokey$cokey, reskinds_by_cokey$mukey, function(x) unique(x))), reskinds = as.character(tapply(reskinds_by_cokey$reskind, reskinds_by_cokey$mukey, function(x) if(all(is.na(x))) {NA} else if (length(unique(x[!is.na(x)]))==1) {unique(x[!is.na(x)])} else{paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')})), stringsAsFactors = FALSE)
-unique(reskinds_by_mukey$reskinds)
-
-#what about rock outcrop
-compnames_by_mukey <- data.frame(mukey=row.names(tapply(comp_data_Fresno$compname, comp_data_Fresno$mukey, function(x) unique(x))), compnames = as.character(tapply(comp_data_Fresno$compname, comp_data_Fresno$mukey, function(x) if(all(is.na(x))) {NA} else if (length(unique(x[!is.na(x)]))==1) {unique(x[!is.na(x)])} else{paste(unique(x[!is.na(x)])[order(unique(x[!is.na(x)]))], collapse = '-')})), stringsAsFactors = FALSE)
+#all compnames by mukey 
+#sum(grepl('-', unique(comp_data_Fresno$compname)))
+compnames_by_mukey <- data.frame(mukey=row.names(tapply(comp_data_Fresno$compname, comp_data_Fresno$mukey, function(x) unique(x))), compnames = as.character(tapply(comp_data_Fresno$compname, comp_data_Fresno$mukey, concat_names)), stringsAsFactors = FALSE)
+unique(compnames_by_mukey$compnames) #422 different map-unit combos accounting for both major and minor components; decat=TRUE has no effect
+dim(compnames_by_mukey) #but 812 mukeys
 fresno_mu_aea$Rock_OC <- ifelse(grepl('Rock outcrop', compnames_by_mukey$compnames[match(fresno_mu_aea$mukey, compnames_by_mukey$mukey)]), 'Yes', 'No')
-summary(as.factor(fresno_mu_aea$Rock_OC)) #1953 have rock outcrop
-fresno_mu_aea$Lithic <- ifelse(grepl('\\bL|lithic\\b', reskinds_by_mukey$reskinds[match(fresno_mu_aea$mukey, reskinds_by_mukey$mukey)]) | fresno_mu_aea$Rock_OC=='Yes', 'Yes', 'No') #5748 was 'yes' after accounting for mukeys with more than one cokey with restrictions and those with rock outrcrop but no components with a subsoil lithic contact
+summary(as.factor(fresno_mu_aea$Rock_OC)) #1953 polygons have rock outcrop
+
+
+fresno_mu_aea$Lithic <- ifelse(grepl('\\bL|lithic\\b', reskinds_by_mukey$reskinds[match(fresno_mu_aea$mukey, reskinds_by_mukey$mukey)]) | fresno_mu_aea$Rock_OC=='Yes', 'Yes', 'No') #5748 were 'yes' after accounting for mukeys with more than one cokey with restrictions and those with rock outrcrop but no major components with a subsoil lithic contact
 summary(as.factor(fresno_mu_aea$Lithic))
 
 #what about rock outcrop? this below needs to be refined to account for percentage of rock outcrop also
@@ -153,7 +181,7 @@ fresno_mu_aea$Lthc_cm <- NA
 fresno_mu_aea$Lthc_cm[fresno_mu_aea$Lithic=='Yes'] <- restrictions_Fresno$resdept_r[restrictions_Fresno$reskind=="Lithic bedrock"][match(fresno_mu_aea$mukey[fresno_mu_aea$Lithic=='Yes'], restrictions_Fresno$mukey[restrictions_Fresno$reskind=="Lithic bedrock"])]
 
 
-sum(grepl('\\bL|lithic\\b', reskinds_by_cokey$reskinds)) #21806; was only 11147 when just lowercase search supplied
+sum(grepl('\\bL|lithic\\b', reskinds_by_cokey$reskinds)) #569 in Fresno only
 #write to file
 shapefile(fresno_mu_aea, file.path(ssurgoDir, 'ca_mapunits/fresno_only/ca651_653_654mu_aea.shp'), overwrite=TRUE)
 
