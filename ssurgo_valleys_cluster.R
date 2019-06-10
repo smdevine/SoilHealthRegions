816a58bfa6e8997d8ec1e479237127e796e4c7c1
smdevine <smdevine@ucdavis.edu>
2019-06-10 16:49:09 -0700

    in progress

--- ssurgo_valleys_cluster.R
+++ ssurgo_valleys_cluster.R
@@ -1,9 +1,14 @@
+#TO-DO
+#(1) run cluster analysis on Salinas only
+#(2) summarize classes for 4 and 5 
 library(raster)
 dataDir <- 'C:/Users/smdevine/Desktop/PostDoc/soil health/summaries/valley_trial'
 list.files(dataDir)
 valley_mu_aea_30cm <- shapefile(file.path(dataDir, 'valley_30cm.shp'))
+names(valley_mu_aea_30cm)
 valley30cm <- read.csv(file.path(dataDir, 'valley_30cm_test.csv'), stringsAsFactors = FALSE)
 dim(valley30cm)
+colnames(valley30cm)
 #acres_by_mukey <- tapply(valley30cm$area_ac, valley30cm$mukey, sum)
 acres_by_mukey <- aggregate(area_ac ~ mukey, data = valley30cm, sum)
 table(valley30cm$mukey)
@@ -17,15 +22,18 @@ valley30cm_by_mukey$area_proportion <- valley30cm_by_mukey$area_ac / sum(valley3
 hist(valley30cm_by_mukey$area_proportion)
 valley30cm_by_mukey[which.max(valley30cm_by_mukey$area_proportion),]
 sum(valley30cm$Drpn_dep == 200, na.rm = TRUE)
-valley30cm_by_mukey$Drpn_dep
 colnames(valley30cm_by_mukey)
 valley30cm_by_mukey$muname[is.na(valley30cm_by_mukey$aws050wta)]
 df_for_clustering <- valley30cm_by_mukey[!is.na(valley30cm_by_mukey$aws050wta), ]
 rownames(df_for_clustering) <- df_for_clustering$mukey
 summary(df_for_clustering)
-df_for_clustering <- df_for_clustering[,c('area_proportion', "Lthc_dep", "Plth_dep", "Drpn_dep", "ATC_dep", "Natr_dep", "Salc_dep", "SCTS_dep", "MRes_dep", 'storiemn', 'clay_30cm', 'om_30cm', 'cec_30cm', 'bd_30cm', 'kwf_30cm', 'ec_30cm', 'pH_30cm', 'sar_30cm', 'lep_30cm', 'ksat_30cm', 'awc_30cm')] #frags have 160 mukeys with NA
+df_for_clustering <- df_for_clustering[,c('area_proportion', 'MnRs_dep', 'clay_30cm', 'om_30cm', 'cec_30cm', 'bd_30cm', 'kwf_30cm', 'ec_30cm', 'pH_30cm', 'sar_30cm', 'lep_30cm', 'ksat_30cm', 'awc_30cm')] #frags have 160 mukeys with NA
+# "storiemn", "Lthc_dep", "Plth_dep", "Drpn_dep", "ATC_dep", "Natr_dep", "Salc_dep", "SCTS_dep", "MRes_dep"
 df_for_clustering_scaled <- scale(df_for_clustering[,2:ncol(df_for_clustering)])
 summary(df_for_clustering_scaled) #awkward
+lapply(df_for_clustering_scaled, hist)
+cluster_2 <- kmeans(na.omit(df_for_clustering_scaled), 2)
+cluster_2
 cluster_3 <- kmeans(na.omit(df_for_clustering_scaled), 3)
 cluster_3
 cluster_4 <- kmeans(na.omit(df_for_clustering_scaled), 4)
@@ -48,18 +56,26 @@ cluster_12 <- kmeans(na.omit(df_for_clustering_scaled), 12)
 cluster_12
 cluster_13 <- kmeans(na.omit(df_for_clustering_scaled), 13)
 cluster_13
-results <- sapply(1:20, function(x) {
-  result <- kmeans(na.omit(df_for_clustering_scaled), x)
- round(100 * result$betweenss / result$totss, 1)})
-plot(1:20, results, type='b')
+kmeans_test <- function(y) {sapply(1:y, function(x) {
+    result <- kmeans(na.omit(df_for_clustering_scaled), x)
+    round(100 * result$betweenss / result$totss, 1)})
+}
+results <- replicate(1000, kmeans_test(20))
+plot(1:20, rowMeans(results), type='b')
+text(1:20, rowMeans(results), labels=as.character(1:20), pos=1, offset=0.5)
 valley30cm_by_mukey$cluster_4 <- cluster_4$cluster[match(valley30cm_by_mukey$mukey, names(cluster_4$cluster))]
-sum(valley30cm_by_mukey$area_ac[!is.na(valley30cm_by_mukey$cluster_4)]) #1535232
-2377819 / 2508000 #94.8% got a cluster if we ignore fragments
+sum(valley30cm_by_mukey$area_ac[!is.na(valley30cm_by_mukey$cluster_4)]) 
+#2377819 / 2508000 #94.8% got a cluster if we ignore fragments
+2415187 / 2508000 #96.2% got a cluster if frags and storie ignored
 
+valley_mu_aea_30cm$cluster_2 <- cluster_2$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_2$cluster))]
+valley_mu_aea_30cm$cluster_3 <- cluster_3$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_3$cluster))]
 valley_mu_aea_30cm$cluster_4 <- cluster_4$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_4$cluster))]
+valley_mu_aea_30cm$cluster_5 <- cluster_5$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_5$cluster))]
 valley_mu_aea_30cm$cluster_6 <- cluster_6$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_6$cluster))]
-valley_mu_aea_30cm$cluster_8 <- cluster_8$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_8$cluster))]
-valley_mu_aea_30cm$cluster_10 <- cluster_10$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_10$cluster))]
-valley_mu_aea_30cm$cluster_13 <- cluster_13$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_13$cluster))]
-shapefile(valley_mu_aea_30cm, file.path(dataDir, 'valley_30cm.shp'), overwrite=TRUE)
+valley_mu_aea_30cm$cluster_7 <- cluster_7$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_7$cluster))]
+# valley_mu_aea_30cm$cluster_8 <- cluster_8$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_8$cluster))]
+# valley_mu_aea_30cm$cluster_10 <- cluster_10$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_10$cluster))]
+# valley_mu_aea_30cm$cluster_13 <- cluster_13$cluster[match(valley_mu_aea_30cm$mukey, names(cluster_13$cluster))]
+shapefile(valley_mu_aea_30cm, file.path(dataDir, 'valley_30cm_cluster.shp'), overwrite=TRUE)
 sum(valley_mu_aea_30cm$area_ac[!is.na(valley_mu_aea_30cm$cluster_4)]) / sum(valley_mu_aea_30cm$area_ac)
