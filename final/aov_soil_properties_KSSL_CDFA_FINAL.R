1a8efe97845bbc320b3a9db51335ca39bf034c38
smdevine <smdevine@ucdavis.edu>
2020-05-04 09:59:09 -0700

    version for draft v1 of paper

--- final/aov_soil_properties_KSSL_CDFA_FINAL.R
+++ final/aov_soil_properties_KSSL_CDFA_FINAL.R
@@ -35,6 +35,8 @@ dom_order_by_mukey <- read.csv(file.path(dataDir, 'FINAL results', "soil survey 
 
 kssl_ssurgo_extract <- read.csv(file.path(ksslDir, 'kssl_pts_ssurgo_30cm_extract_FINAL.csv'), stringsAsFactors = FALSE)
 kssl_points_30cm <- read.csv(file.path(ksslDir, 'kssl_cluster_30cm_FINAL.csv'), stringsAsFactors = FALSE) #updated 2/11/20 to consider total C when org C not available but soil pH sufficiently low for calculating SOC content
+kssl_points_30cm$labsampnum <- kssl_ssurgo_extract$labsampnum[match(kssl_points_30cm$pedon_key, kssl_ssurgo_extract$pedon_key)]
+sum(grepl('UCD', kssl_points_30cm$labsampnum)) #54
 colnames(kssl_points_30cm)
 dim(kssl_points_30cm)
 kssl_points_30cm$mukey <- kssl_ssurgo_extract$mukey[match(kssl_points_30cm$pedon_key, kssl_ssurgo_extract$pedon_key)]
@@ -63,8 +65,11 @@ sum(is.na(kssl_points_30cm$om_30cm_v2))
 sum(is.na(kssl_points_30cm$om_30cm))
 kssl_points_30cm$om_30cm <- kssl_points_30cm$om_30cm_v2
 kssl_points_30cm$om_30cm_v2 <- NULL
-
-
+kssl_points_30cm[139:150,]
+valley30cm_by_mukey[valley30cm_by_mukey$mukey==460870,]
+dom_order_by_mukey[dom_order_by_mukey$mukey==460870,]
+sum(is.na(dom_order_by_mukey$dom_order)) #89
+lapply(kssl_points_30cm, function(x) sum(!is.na(x)))
 
 # kssl_points_100cm <- read.csv(file.path(ksslDir, 'kssl_cluster_100cm_NArm.csv'), stringsAsFactors = FALSE)
 # colnames(kssl_points_100cm)
@@ -82,16 +87,39 @@ kerri_points_30cm <- read.csv(file.path(kerriDir, 'FINAL', 'CDFA_samples_cluster
 colnames(kerri_points_30cm)
 kerri_points_30cm$om_30cm <- kerri_points_30cm$totC_30cm * om_to_oc
 kerri_ssurgo_extract <- read.csv(file.path(kerriDir, 'FINAL', 'CDFA_pts_ssurgo_30cm_extract_FINAL.csv'), stringsAsFactors = FALSE)
+colnames(kerri_ssurgo_extract)
 kerri_points_30cm$mukey <- kerri_ssurgo_extract$mukey[match(kerri_points_30cm$Concatenate, kerri_ssurgo_extract$Concatenate)]
 kerri_points_30cm$dom_order <- dom_order_by_mukey$dom_order[match(kerri_points_30cm$mukey, dom_order_by_mukey$mukey)]
 tapply(kerri_points_30cm$om_30cm, kerri_points_30cm$dom_order, summary)
 table(kerri_points_30cm$dom_order)
+
+kerri_metadata <- read.csv(file.path(kerriDir, 'CDFA Soil Survey All Data_copy.csv'), stringsAsFactors = FALSE)
+unique(kerri_metadata$Area)
+length(unique(kerri_metadata$Concatenate))#127 unique points, 102 in study area
+
+kerri_points_30cm$vineyard_region <- kerri_metadata$Area[match(kerri_points_30cm$Concatenate, kerri_metadata$Concatenate)]
+kerri_points_30cm$vineyard_name <- kerri_metadata$Vineyard.Management[match(kerri_points_30cm$Concatenate, kerri_metadata$Concatenate)]
+table(kerri_points_30cm$vineyard_region) #30 in Lodi, 97 in Napa
+table(kerri_points_30cm$vineyard_region[!is.na(kerri_points_30cm$cluster_7)]) #27 in Lodi, 76 in Napa in area of interest
+unique(kerri_points_30cm$vineyard_name[kerri_points_30cm$vineyard_region=='Napa']) #16 vineyards in Napa (paper says ninteen)
+unique(kerri_points_30cm$vineyard_name[kerri_points_30cm$vineyard_region=='Lodi']) #5 vineyards (paper says nine)
+tapply(kerri_points_30cm$Concatenate[kerri_points_30cm$vineyard_region=='Napa'], kerri_points_30cm$vineyard_name[kerri_points_30cm$vineyard_region=='Napa'], function(x) length(x))
+tapply(kerri_points_30cm$Concatenate[kerri_points_30cm$vineyard_region=='Lodi'], kerri_points_30cm$vineyard_name[kerri_points_30cm$vineyard_region=='Lodi'], function(x) length(x))
+kerri_points_30cm[kerri_points_30cm$vineyard_name=='Big Ranch Vineyard',]
+tapply(kerri_points_30cm$cluster_7, kerri_points_30cm$vineyard_name, function(x) length(unique(x)))
+tapply(kerri_points_30cm$cluster_7, kerri_points_30cm$vineyard_name, function(x) unique(x))
+tapply(kerri_points_30cm$cluster_7, kerri_points_30cm$vineyard_region, function(x) unique(x))
+tapply(kerri_points_30cm$totC_30cm, kerri_points_30cm$vineyard_region, function(x) length(unique(x)))
 
 df_combined2 <- kerri_points_30cm[!is.na(kerri_points_30cm$cluster_7), c('clay_30cm', 'om_30cm', 'bd_30cm', 'pH_H2O_30cm', 'kgOrg.m2_30cm', colnames(kerri_points_30cm)[grepl('cluster_', colnames(kerri_points_30cm))], 'dom_order')]
 dim(df_combined2) #103 rows
 df_combined2$source <- 'CDFA'
 
 df_combined <- rbind(df_combined1, df_combined2)
+dim(df_combined) #472
+table(df_combined$dom_order[!is.na(df_combined$om_30cm)])
+df_combined$om_30cm[df_combined$dom_order=='Ultisols']
+lapply(df_combined, function(x) sum(!is.na(x)))
 
 clus_9_names <- c('4. Coarse w/pans', '5. Loamy w/pans', '3. Loamy w/no restrictions', '1. Very coarse w/no restrictions',  '7. Coarse saline-sodic', '2. Coarse w/no restrictions', '8. Fine saline-sodic', '6. Loamy w/pans (high OM)', '9. Fine shrink-swell') #order corrected 4/7/20
 
@@ -111,6 +139,9 @@ test[[1]][,4]
 compare_region_means <- function(df, names, cluster_no, y, alpha) {
   x <- names[match(df[[paste0('cluster_', cluster_no)]], 1:cluster_no)]
   y_vector <- df[[y]]
+  print('These points have data (by soil order)')
+  print(table(x[!is.na(y_vector)]))
+  print(paste('Total of', sum(table(x[!is.na(y_vector)])), 'points with data.'))
   print(paste(sum(TukeyHSD(aov(y_vector ~ x))[[1]][,4] <= alpha), 'out of ', length(TukeyHSD(aov(y_vector ~ x))[[1]][,4]), 'are significant.'))
   # print(paste0(y, '_cluster_', as.character(cluster_no), '_aov_results.txt'))
   if(!dir.exists(file.path(resultsDir, paste(as.character(cluster_no), 'region')))) {dir.create(file.path(resultsDir, paste(as.character(cluster_no), 'region')))}
@@ -126,7 +157,7 @@ compare_region_means <- function(df, names, cluster_no, y, alpha) {
 }
 
 #9 region
-compare_region_means(df=df_combined, names=clus_9_names, cluster_no=9, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_9_names, cluster_no=9, y='clay_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_9_names, cluster_no=9, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_9_names, cluster_no=9, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_9_names, cluster_no=9, y='pH_H2O_30cm', alpha = 0.05)
@@ -138,7 +169,7 @@ compare_region_means(df=df_combined, names=clus_9_names, cluster_no=9, y='kgOrg.
 
 
 #8 region
-compare_region_means(df=df_combined, names=clus_8_names, cluster_no=8, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_8_names, cluster_no=8, y='clay_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_8_names, cluster_no=8, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_8_names, cluster_no=8, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_8_names, cluster_no=8, y='pH_H2O_30cm', alpha = 0.05)
@@ -149,7 +180,7 @@ compare_region_means(df=kssl_points_30cm, names=clus_8_names, cluster_no=8, y='a
 compare_region_means(df=df_combined, names=clus_8_names, cluster_no=8, y='kgOrg.m2_30cm', alpha = 0.05)
 
 #7 region
-compare_region_means(df=df_combined, names=clus_7_names, cluster_no=7, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_7_names, cluster_no=7, y='clay_30cm', alpha = 0.05) #kssl only
 compare_region_means(df=df_combined, names=clus_7_names, cluster_no=7, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_7_names, cluster_no=7, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_7_names, cluster_no=7, y='pH_H2O_30cm', alpha = 0.05) #verified that re-aggregation on 2/11/20 did not change soil pH results (only OM and kg SOC)
@@ -191,7 +222,7 @@ meankgOC_clus7 <- tapply(kssl_points_30cm$kgOrg.m2_30cm, clus_7_names[match(kssl
 meankgOC_clus7[order(meankgOC_clus7)]
 
 #6 region
-compare_region_means(df=df_combined, names=clus_6_names, cluster_no=6, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_6_names, cluster_no=6, y='clay_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_6_names, cluster_no=6, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_6_names, cluster_no=6, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_6_names, cluster_no=6, y='pH_H2O_30cm', alpha = 0.05)
@@ -224,7 +255,7 @@ meanAWC_clus6 <- tapply(kssl_points_30cm$awc_30cm, clus_6_names[match(kssl_point
 meanAWC_clus6[order(meanAWC_clus6)]
 
 #5 region
-compare_region_means(df=df_combined, names=clus_5_names, cluster_no=5, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_5_names, cluster_no=5, y='clay_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_5_names, cluster_no=5, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_5_names, cluster_no=5, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_5_names, cluster_no=5, y='pH_H2O_30cm', alpha = 0.05)
@@ -236,7 +267,7 @@ compare_region_means(df=df_combined, names=clus_5_names, cluster_no=5, y='kgOrg.
 
 
 #4 region
-compare_region_means(df=df_combined, names=clus_4_names, cluster_no=4, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_4_names, cluster_no=4, y='clay_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_4_names, cluster_no=4, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_4_names, cluster_no=4, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_4_names, cluster_no=4, y='pH_H2O_30cm', alpha = 0.05)
@@ -247,7 +278,7 @@ compare_region_means(df=kssl_points_30cm, names=clus_4_names, cluster_no=4, y='a
 compare_region_means(df=df_combined, names=clus_4_names, cluster_no=4, y='kgOrg.m2_30cm', alpha = 0.05)
 
 #3 region
-compare_region_means(df=df_combined, names=clus_3_names, cluster_no=3, y='clay_30cm', alpha = 0.05)
+compare_region_means(df=df_combined1, names=clus_3_names, cluster_no=3, y='clay_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_3_names, cluster_no=3, y='om_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_3_names, cluster_no=3, y='bd_30cm', alpha = 0.05)
 compare_region_means(df=df_combined, names=clus_3_names, cluster_no=3, y='pH_H2O_30cm', alpha = 0.05)
@@ -258,7 +289,15 @@ compare_region_means(df=kssl_points_30cm, names=clus_3_names, cluster_no=3, y='a
 compare_region_means(df=df_combined, names=clus_3_names, cluster_no=3, y='kgOrg.m2_30cm', alpha = 0.05)
 
 #make group contrast by cluster figure
-contrast_summary <- data.frame(cluster_no=3:9, contrast_no=c(3,6,10,15,21,28,36), clay=c(3,5,7,8,14,17,19), om=c(2,4,6,9,11,13,17), bd=c(0,0,2,2,2,3,2), pH=c(2,6,9,12,16,18,18), cec=c(3,5,8,9,13,16,16), lep=c(2,3,6,7,11,11,11), ec=c(1,3,2,1,5,6,6), kgSOC=c(2,4,5,8,11,8,12))
+contrast_summary <- data.frame(cluster_no=3:9, contrast_no=c(3,6,10,15,21,28,36), clay=c(3,4,7,8,14,17,17), om=c(2,4,6,9,11,13,17), bd=c(0,0,2,2,2,3,2), pH=c(2,6,9,12,16,18,18), cec=c(3,5,8,9,13,16,16), lep=c(2,3,6,7,11,11,11), ec=c(1,3,2,1,5,6,6), kgSOC=c(2,4,5,8,11,8,12)) #updated on 4/27/20 so that clay analysis only includes KSSL data
+
+mean(sapply(contrast_summary[1,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[1], 2)})) #0.63 for 3
+mean(sapply(contrast_summary[2,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[2], 2)})) #0.63 for 4
+mean(sapply(contrast_summary[3,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[3], 2)})) #0.56 for 5
+mean(sapply(contrast_summary[4,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[4], 2)})) #0.47 for 6
+mean(sapply(contrast_summary[5,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[5], 2)})) #0.49 for 7
+mean(sapply(contrast_summary[6,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[6], 2)})) #0.41 for 8
+mean(sapply(contrast_summary[7,3:ncol(contrast_summary)], function(x) {round(x/contrast_summary$contrast_no[7], 2)})) #0.35
 
 tiff(file = file.path(FiguresDir, 'FINAL', 'sig_group_contrasts_3to9.tif'), family = 'Times New Roman', width = 6.5, height = 5, pointsize = 11, units = 'in', res=800, compression = 'lzw')
 par(mar=c(4,4,4.5,1))
@@ -315,7 +354,10 @@ TukeyHSD()
 compare_SoilOrder_means <- function(df, y, alpha) {
   y_vector <- df[[y]]
   x <- df[['dom_order']]
-  print(paste(sum(TukeyHSD(aov(y_vector ~ x))[[1]][,4] <= alpha), 'out of ', length(TukeyHSD(aov(y_vector ~ x))[[1]][,4]), 'are significant.'))
+  print('These points have data (by soil order)')
+  print(table(x[!is.na(y_vector)]))
+  print(paste('Total of', sum(table(x[!is.na(y_vector)])), 'points with data.'))
+  print(paste(sum(TukeyHSD(aov(y_vector ~ x))[[1]][,4] <= alpha), 'out of ', length(TukeyHSD(aov(y_vector ~ x))[[1]][,4]), 'possible contrasts are significant.'))
   sink(file=file.path(resultsDir, 'SoilOrders', paste0(y, '_SoilOrder_aov_results.txt')))
   print(paste('ANOVA and Tukey HSD results for', y, 'and soil orders'))
   print(paste(sum(TukeyHSD(aov(y_vector ~ x))[[1]][,4] <= alpha), 'out of ', length(TukeyHSD(aov(y_vector ~ x))[[1]][,4]), 'are significant.'))
@@ -327,7 +369,7 @@ compare_SoilOrder_means <- function(df, y, alpha) {
   # model.tables(aov(y_vector ~ x))
 }
 
-compare_SoilOrder_means(df=df_combined, y='clay_30cm', alpha = 0.05)
+compare_SoilOrder_means(df=df_combined1, y='clay_30cm', alpha = 0.05) #kssl only
 compare_SoilOrder_means(df=df_combined, y='om_30cm', alpha = 0.05)
 compare_SoilOrder_means(df=df_combined, y='bd_30cm', alpha = 0.05)
 compare_SoilOrder_means(df=df_combined, y='pH_H2O_30cm', alpha = 0.05) #verified that re-aggregation on 2/11/20 did not change soil pH results (only OM and kg SOC)
@@ -336,3 +378,10 @@ compare_SoilOrder_means(df=kssl_points_30cm, y='lep_30cm', alpha = 0.05)
 compare_SoilOrder_means(df=kssl_points_30cm, y='ec_30cm', alpha = 0.05)
 compare_SoilOrder_means(df=kssl_points_30cm, y='awc_30cm', alpha = 0.05)
 compare_SoilOrder_means(df=df_combined, y='kgOrg.m2_30cm', alpha = 0.05)
+
+pH_by_order <- tapply(df_combined$pH_H2O_30cm, df_combined$dom_order, mean, na.rm=TRUE)
+pH_by_order[order(pH_by_order)]
+om_by_order <- tapply(df_combined$om_30cm, df_combined$dom_order, mean, na.rm=TRUE)
+om_by_order[order(om_by_order)]
+clay_by_order <- tapply(df_combined$clay_30cm, df_combined$dom_order, mean, na.rm=TRUE)
+clay_by_order[order(clay_by_order)]
