3a77919c9836091a95a047625a175ee36ff7618b
smdevine <smdevine@ucdavis.edu>
2021-07-12 12:36:42 -0700

    added pdf export for CalAg Figure 4

--- CalAg/SOM_barchart_0_30_longterm.R
+++ CalAg/SOM_barchart_0_30_longterm.R
@@ -27,3 +27,21 @@ text(x=7.4, y=2.05, 'SOCS')
 text(x=14.8, y=1.75, 'LTRAS')
 dev.off()
 bar_dims
+
+cex_labels <- 0.8
+cairo_pdf(file = file.path(FiguresDir, 'PDF versions', 'Figure_4_FINAL.pdf'), pointsize = 12, family = 'Times New Roman', width = 6, height = 4)
+par(mar=c(3,3.5,0.1,0.1))
+bar_dims <- barplot(SOM_data$SOM_start, beside=TRUE, space=c(0.1,0.1,0.1,0.1,0.3,0.1,0.1,0.1,0.1,0.3,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1), col=SOM_data$bar_color, ylab='', ylim=c(0,2.25), cex.names=1) #inset=c(0,-0.35) to place legend outside plot
+legend(x=15.5, y=1.75, legend = c('region 1', 'region 2'),  col=c('lightgoldenrod', 'tan4'), lty=2, lwd=2, xjust=0, yjust=0, cex = 0.9, title = 'KSSL median SOM', bty='n') #
+barplot(SOM_data$SOM_end, beside=TRUE, space=c(0.1,0.1,0.1,0.1,0.3,0.1,0.1,0.1,0.1,0.3,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1), border=TRUE, angle = 45, density = 10, col='black', ylab='', ylim=c(0,2.25), axes=FALSE, cex.names=1, add = TRUE)
+mtext('0-12" (0-30 cm) soil organic matter (%)', side = 2, line = 2.25)
+mtext(SOM_data$line1, side=1, line=-0.15, at=bar_dims, cex=cex_labels)
+mtext(SOM_data$line2, side=1, line=0.5, at=bar_dims, cex=cex_labels)
+mtext(SOM_data$line3, side=1, line=1.15, at=bar_dims, cex=cex_labels)
+mtext(SOM_data$line4, side=1, line=1.8, at=bar_dims, cex=cex_labels)
+abline(h=1.65, col='tan4', lty=2, lwd=2)
+abline(h=1.07, col='lightgoldenrod', lty=2, lwd=2)
+text(x=2.3, y=1.75, 'WSREC')
+text(x=7.4, y=2.05, 'SOCS')
+text(x=14.8, y=1.75, 'LTRAS')
+dev.off()
